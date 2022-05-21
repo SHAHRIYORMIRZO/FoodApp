febeb429b277e7fa4f9049cc332371f84256161e
SHAHRIYORMIRZO <sakbaraliyebv8@gmail.com>
2022-05-21 20:15:37 +0500

    FoodApp project

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () =>{
+    return(
+      setCartIsShown(true)
+    );
+  };
+
+  const hideCartHandler = () =>{
+    return(
+      setCartIsShown(false)
+    );
+  };
+  
+  return (
+    <CartProvider>
+      <Header onShownCart={showCartHandler} />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
